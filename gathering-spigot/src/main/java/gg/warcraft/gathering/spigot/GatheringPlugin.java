88cad287e8d44b4732a634a29b1cd416ed84949a
Yannick <fishb6nes@gmail.com>
2018-07-08 19:43:14 +0100

    Remove service loader configuration

--- gathering-spigot/src/main/java/gg/warcraft/gathering/spigot/GatheringPlugin.java
+++ gathering-spigot/src/main/java/gg/warcraft/gathering/spigot/GatheringPlugin.java
@@ -1,7 +1,23 @@
 package gg.warcraft.gathering.spigot;
 
+import com.google.inject.AbstractModule;
+import gg.warcraft.monolith.api.Monolith;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GatheringPlugin extends JavaPlugin {
 
+    @Override
+    public void onLoad() {
+        saveDefaultConfig();
+        FileConfiguration localConfig = getConfig();
+
+        AbstractModule spigotGatheringModule = new SpigotGatheringModule();
+        Monolith.registerModule(spigotGatheringModule);
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
 }
