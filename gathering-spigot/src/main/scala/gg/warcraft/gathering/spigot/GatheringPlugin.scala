50301686d669eae1d638ead4480b8f84e943efb6
fishb6nes <fishb6nes@users.noreply.github.com>
2020-02-29 12:20:07 +0000

    Cleanup plugin initialization

--- gathering-spigot/src/main/scala/gg/warcraft/gathering/spigot/GatheringPlugin.scala
+++ gathering-spigot/src/main/scala/gg/warcraft/gathering/spigot/GatheringPlugin.scala
@@ -1,53 +1,45 @@
 package gg.warcraft.gathering.spigot
 
+import gg.warcraft.gathering.{GatheringConfig, GatheringSpotService}
 import gg.warcraft.gathering.gatherable.{
   BlockGatherableEventHandler, BlockGatherableService, EntityGatherableEventHandler,
-  EntityGatherableService
+  EntityGatherableRepository, EntityGatherableService
 }
-import gg.warcraft.gathering.{GatheringConfig, GatheringSpotService}
-import gg.warcraft.monolith.api.Implicits.{eventService, _}
-import gg.warcraft.monolith.api.core.TaskService
-import gg.warcraft.monolith.api.core.event.EventService
-import gg.warcraft.monolith.api.entity.player.service.PlayerQueryService
-import gg.warcraft.monolith.api.entity.service.{
-  EntityCommandService, EntityQueryService
-}
-import gg.warcraft.monolith.api.world.block.backup.BlockBackupService
+import gg.warcraft.monolith.spigot.Implicits._
+import io.circe.generic.auto._
+import io.circe.parser.decode
 import org.bukkit.plugin.java.JavaPlugin
 
 class GatheringPlugin extends JavaPlugin {
   override def onLoad(): Unit = saveDefaultConfig()
 
   override def onEnable(): Unit = {
-    val config: GatheringConfig = null // TODO
-    init(config)
-  }
+    // spot implicits
+    implicit val gatheringSpotService: GatheringSpotService =
+      new GatheringSpotService
 
-  private def init(
-      config: GatheringConfig
-  )(
-      implicit eventService: EventService,
-      taskService: TaskService,
-      blackBackupService: BlockBackupService,
-      entityService: EntityQueryService,
-      entityCommandService: EntityCommandService,
-      playerService: PlayerQueryService
-  ): Unit = {
-    val gatheringSpotService = new GatheringSpotService
-    config.gatheringSpots
-      .map(_.toGatheringSpot)
-      .foreach(gatheringSpotService.addGatheringSpot)
-
-    val blockGatherableHandler = new BlockGatherableEventHandler(
-      gatheringSpotService,
+    // block implicits
+    implicit val blockGatherableService: BlockGatherableService =
       new BlockGatherableService
-    )
-    eventService.subscribe(blockGatherableHandler)
 
-    val entityGatherableHandler = new EntityGatherableEventHandler(
-      gatheringSpotService,
+    // entity implicits
+    implicit val entityGatherableRepository: EntityGatherableRepository =
+      new EntityGatherableRepository
+    implicit val entityGatherableService: EntityGatherableService =
       new EntityGatherableService
-    )
-    eventService.subscribe(entityGatherableHandler)
+
+    // parse config
+    decode[GatheringConfig](getConfig.saveToString()) match {
+      case Left(err) =>
+        getLogger.severe("Failed to parse configuration: " + err.getMessage)
+      case Right(config) =>
+        config.gatheringSpots
+          .map(_.toGatheringSpot)
+          .foreach(gatheringSpotService.addGatheringSpot)
+    }
+
+    // init
+    eventService.subscribe(new BlockGatherableEventHandler)
+    eventService.subscribe(new EntityGatherableEventHandler)
   }
 }
