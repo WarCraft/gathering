2fa0843e4b820e83fc388c24c4a0484aa209d2f5
fishb6nes <fishb6nes@users.noreply.github.com>
2020-05-19 20:32:20 +0100

    Fix missing evidence parameter in config decoding

--- gathering-spigot/src/main/scala/gg/warcraft/gathering/spigot/GatheringPlugin.scala
+++ gathering-spigot/src/main/scala/gg/warcraft/gathering/spigot/GatheringPlugin.scala
@@ -1,14 +1,14 @@
 package gg.warcraft.gathering.spigot
 
 import gg.warcraft.gathering.GatheringConfig
-import gg.warcraft.gathering.gatherable.{
-  BlockGatherableEventHandler, EntityGatherableEventHandler
-}
+import gg.warcraft.gathering.gatherable.{ BlockGatherableEventHandler, EntityGatherableEventHandler }
+import gg.warcraft.monolith.api.core.Codecs.Circe.enumDecoder
+import gg.warcraft.monolith.api.world.World
 import gg.warcraft.monolith.spigot.SpigotMonolithPlugin
 import gg.warcraft.monolith.spigot.implicits._
 import io.circe.generic.auto._
-import io.circe.parser._
-import io.getquill.{SnakeCase, SqliteDialect}
+import io.circe.Decoder
+import io.getquill.{ SnakeCase, SqliteDialect }
 
 class GatheringPlugin extends SpigotMonolithPlugin {
   import implicits._
@@ -24,12 +24,10 @@ class GatheringPlugin extends SpigotMonolithPlugin {
   }
 
   override def onEnable(): Unit = {
-    decode[GatheringConfig](getConfig.saveToString()) match {
-      case Left(err) =>
-        getLogger.severe(s"Failed to parse configuration with $err")
-      case Right(config) =>
-        config.gatheringSpots.foreach(gatheringSpotService.addGatheringSpot)
-    }
+    implicit val worldDec: Decoder[World] = enumDecoder(World.valueOf)
+
+    val config = parseConfig[GatheringConfig](getConfig.saveToString())
+    config.gatheringSpots.foreach(gatheringSpotService.addGatheringSpot)
 
     eventService.subscribe(new BlockGatherableEventHandler)
     eventService.subscribe(new EntityGatherableEventHandler)
